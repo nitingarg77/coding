ec0aae045e32be8a62223f8cefbffaa01f96d19f
nitingarg77 <nitingarg77@users.noreply.github.com>
2026-01-02 18:49:13 +0530

    Add initial implementation of main.py with variable definitions and print statements

---
+++ main.py
@@ -0,0 +1,17 @@
+# variable = A container for a value(string, number, boolean, etc.) A variable behaves as the name for that value.
+first_name = "Nitin"
+food = "Pizza"
+email = "nitin@email.com"
+# these are string variables
+print(f"Hello {first_name}")
+print(f"You like {food}")   
+print(f"Your email is: {email}")
+
+#integer variable
+age = 30
+quantity = 3
+year = 2024
+no_of_students = 30
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"your class has {no_of_students} students in the year {year}")
