d77bcada9fb83fcf9cbcf2f6d1134d4f923b6c8c
nitin <nitingarg77@gmail.com>
2026-01-03 18:25:48 +0530

    after video

---
+++ pylearn.py/string.py
@@ -0,0 +1,17 @@
+#string method
+
+
+
+
+name = "nitin garg" #input("Please enter your name:")
+#result = len(name)
+#result = name.rfind("g")
+#result = name.isalpha()
+#result = name.count("g")
+#result = name.replace("-", "")
+number = name.index("a")
+index = name[0:3:1]
+
+
+print(name[:7])
+#print(index)  # returns -1 if not found
