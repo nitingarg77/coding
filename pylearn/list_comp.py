6e6cf3a0eac3bde8d4bd10ab8c78a25e2f68d467
nitingarg77 <nitingarg77@users.noreply.github.com>
2026-01-06 12:00:04 +0530

    after videos on list comprehensions

---
+++ pylearn/list_comp.py
@@ -0,0 +1,4 @@
+
+nums = [1,-2,3,-4,5,-6,7,-8,9,-10,9,8,7,6,5,4,3,2,1,0]
+pos_nums = [num for num in nums if num > 0]
+print(pos_nums)
---
+++ pylearn/match_case.py
@@ -0,0 +1,19 @@
+def day_of_week(day):
+    match day:
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+        case 7:
+            return "Sunday"
+        case _:
+            return "Invalid day"
+print(day_of_week(3))  # Output: Wednesday
