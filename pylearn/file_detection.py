573e27acc90ef6846fab3f4fdb7b0664440726d9
nitin <nitingarg77@gmail.com>
2026-01-08 16:06:44 +0530

    version changes

--- pylearn/file_detection.py
+++ pylearn/file_detection.py
@@ -2,9 +2,12 @@
 
 import os
 
-file_path = "~/tech/coding/pylearn/2D_list.py"
+file_path = "/home/nitin/tech/coding/pylearn/1test.txt"
 
 if os.path.exists(file_path):
     print(f"the location '{file_path}' exits.")
 else:
     print(f"the location '{file_path}' does not exists. ")
+
+
+print(os.path)
